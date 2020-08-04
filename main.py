0e2e6073f9ef67efcc2c0d08457c94b47ef3c80a
mohammad <m.mohammadi9812@gmail.com>
2020-08-04 22:21:07 +0430

    Initial commit

---
+++ main.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+# Used this link for authentication:
+# https://medium.com/@nickpgott/how-to-login-to-a-reddit-account-with-praw-when-2fa-is-enabled-4db9e82448a5
+
+from dotenv import load_dotenv as load
+import praw
+
+import os
+import random
+import webbrowser
+import socket
+import pandas as pd
+
+load()
+
+client_id      = os.getenv("CLIENT_ID")
+client_secret  = os.getenv("CLIENT_SECRET")
+user_agent     = os.getenv("USER_AGENT")
+username       = os.getenv("REDDIT_USER_NAME")
+password       = os.getenv("REDDIT_PASWORD")
+
+reddit = praw.Reddit(client_id=client_id,
+                     client_secret=client_secret,
+                     user_agent=user_agent,
+                     username=username,
+                     password=password,
+                     redirect_uri="http://localhost:8080")
+
+
+def receive_connection():
+    """
+    Wait for and then return a connected socket..
+    Opens a TCP connection on port 8080, and waits for a single client.
+    """
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('localhost', 8080))
+    server.listen(1)
+    client = server.accept()[0]
+    server.close()
+    return client
+
+
+def send_message(client, message):
+    """
+    Send message to client and close the connection.
+    """
+    client.send('HTTP/1.1 200 OK\r\n\r\n{}'.format(message).encode('utf-8'))
+    client.close()
+
+
+def authenticate():
+    """
+    Authenticate application for user access
+    """
+    state = str(random.randint(0, 65000))
+    scopes = ['identity', 'history', 'read', 'edit']
+    url = reddit.auth.url(scopes, state, 'permanent')
+    webbrowser.open(url)
+
+    client = receive_connection()
+    data = client.recv(1024).decode('utf-8')
+
+    param_tokens = data.split(' ', 2)[1].split('?', 1)[1].split('&')
+    params = {key: value for (key, value) in [token.split('=')
+                                              for token in param_tokens]}
+
+    if state != params['state']:
+        send_message(client, f'State mismatch. Expected: {state} Received: {params["state"]}')
+        return 1
+    elif 'error' in params:
+        send_message(client, params['error'])
+        return 1
+
+    refresh_token = reddit.auth.authorize(params["code"])
+    send_message(client, f"Refresh token: {refresh_token}")
+
+    print(refresh_token)
+    return 0
+
+
+def main():
+    authenticate()
+    savedsubs = reddit.user.me().saved(limit=None)
+    posts, comments = [], []
+    for s in savedsubs:
+        if isinstance(s, praw.models.Submission):
+            posts.append(
+                {
+                    'id': s.id,
+                    'subreddit': s.subreddit.display_name,
+                    'title': s.title,
+                    'permalink': s.permalink,
+                    'score': s.score,
+                    'url': s.url
+                }
+            )
+        elif isinstance(s, praw.models.Comment):
+            comments.append(
+                {
+                    'id': s.id,
+                    'subreddit': s.subreddit.display_name,
+                    'body': s.body,
+                    'permalink': s.permalink,
+                    'score': s.score,
+                    'parent_permalink': s.submission.permalink
+                }
+            )
+    postsdf = pd.DataFrame(posts)
+    commentsdf = pd.DataFrame(comments)
+    return postsdf, commentsdf
+
+
+if __name__ == '__main__':
+    postsdf, commentsdf = main()
+
+    postsdf.to_csv('saved_posts.csv')
+    postsdf.to_excel('saved_posts.xls')
+
+    commentsdf.to_csv('saved_comments.csv')
+    commentsdf.to_excel('saved_comments.xls')
